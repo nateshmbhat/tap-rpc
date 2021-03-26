bf3c659e490e051a1255cbe3ff42f3e9dd352ebf
nateshmbhat <nateshmbhat1@gmail.com>
2021-03-26 20:08:36 +0530

    updated webpack config

---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+const sveltePreprocess = require('svelte-preprocess')
+
+module.exports = {
+  preprocess: sveltePreprocess({
+    defaults: {
+      script: 'typescript',
+    },
+    scss: {
+      includePaths: ['app/theme'],
+    },
+  }),
+  hotReload: false,
+}
